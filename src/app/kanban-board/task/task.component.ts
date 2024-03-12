c8f37995efe2f21c6f63855809080d73189d2cf9
arnk <artur.nowak@gft.com>
2024-03-12 12:33:27 +0100

    Add first css styles for small devices

--- src/app/kanban-board/task/task.component.ts
+++ src/app/kanban-board/task/task.component.ts
@@ -1,14 +1,24 @@
 import { Component, input } from '@angular/core';
 import Task from '../models/task.model';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
 
   task = input<Task>();
+
+  randomColor() {
+    let color = '#';
+    const letters = '0123456789ABCDEF';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
 }
