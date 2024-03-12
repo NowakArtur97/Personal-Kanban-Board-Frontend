67716f0e3125593cfeb6bdad7f500cf7d4de2614
arnk <artur.nowak@gft.com>
2024-03-12 12:57:22 +0100

    Change background colors of task columns and tasks elements

--- src/app/kanban-board/task-column/task-column.component.ts
+++ src/app/kanban-board/task-column/task-column.component.ts
@@ -3,11 +3,12 @@ import { TaskStatus } from '../models/task-status.model';
 import { TaskService } from '../services/task.service';
 import Task from '../models/task.model';
 import { TaskComponent } from '../task/task.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-task-column',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [NgStyle, TaskComponent],
   templateUrl: './task-column.component.html',
   styleUrl: './task-column.component.css'
 })
@@ -16,11 +17,12 @@ export class TaskColumnComponent {
   private taskService = inject(TaskService);
 
   tasksStatus = input<TaskStatus>();
-
   tasks = computed(() =>
     this.taskService.tasks().filter(task =>
       this.hasSameTaskStatus(task)
       || this.hasSameStatusInAnySubtask(task)));
+
+  color = this.randomColor();
 
   // TODO: Remove
   constructor() {
@@ -29,13 +31,18 @@ export class TaskColumnComponent {
     });
   }
 
+  get status(): string {
+    return TaskStatus[this.tasksStatus()!];
+  }
+
   private hasSameStatusInAnySubtask = (task: Task): boolean =>
     task.subtasks.some(subtask => TaskStatus[subtask.status] === this.status);
 
   private hasSameTaskStatus = (task: Task): boolean =>
     TaskStatus[task.status] === this.status;
 
-  get status(): string {
-    return TaskStatus[this.tasksStatus()!];
-  }
+  private randomColor(): string {
+    const colors = ["#ef7d57", "#41a6f6", "#566c86"];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
 }
