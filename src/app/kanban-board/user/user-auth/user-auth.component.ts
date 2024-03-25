c71d4c30e5359fc137abb1ebd78d0f6806ba12b9
arnk <artur.nowak@gft.com>
2024-03-25 18:03:14 +0100

    Set change action button class and input placeholder based on current action

--- src/app/kanban-board/user/user-auth/user-auth.component.ts
+++ src/app/kanban-board/user/user-auth/user-auth.component.ts
@@ -1,9 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-auth',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './user-auth.component.html',
   styleUrl: './user-auth.component.css'
 })
