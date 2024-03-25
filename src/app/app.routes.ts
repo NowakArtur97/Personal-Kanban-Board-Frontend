060d3061de0ea854631f2d1be1d84aca098fe969
arnk <artur.nowak@gft.com>
2024-03-25 12:35:18 +0100

    Change component displayed by default to user auth component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
 import { tasksResolver } from './kanban-board/task/guards/tasks.resolver';
+import { UserAuthComponent } from './kanban-board/user/user-auth/user-auth.component';
 
 export const routes: Routes = [
     {
         path: "",
-        component: AppComponent,
+        component: UserAuthComponent,
+        title: "Personal Kanban Board",
+    },
+    {
+        path: "kanban-board",
+        component: KanbanBoardComponent,
         title: "Personal Kanban Board",
         resolve: { tasks: tasksResolver }
     }
