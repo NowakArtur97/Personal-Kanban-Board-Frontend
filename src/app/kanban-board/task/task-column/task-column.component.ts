5a8ab63d9a53c63f4a79019383b313c82154a4c7
arnk <nowart97@gmail.com>
2024-03-30 16:41:12 +0100

    Populate task columns with tasks

--- src/app/kanban-board/task/task-column/task-column.component.ts
+++ src/app/kanban-board/task/task-column/task-column.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, effect, inject, input } from '@angular/core';
 import { TaskStatus } from '../models/task-status.model';
 import { TaskService } from '../services/task.service';
 import Task from '../models/task.model';
+import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-task-column',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './task-column.component.html',
   styleUrl: './task-column.component.css'
 })
@@ -16,11 +17,12 @@ export class TaskColumnComponent {
 
   tasksStatus = input<TaskStatus>();
 
-  private tasks = computed(() =>
+  tasks = computed(() =>
     this.taskService.tasks().filter(task =>
       this.hasSameTaskStatus(task)
       || this.hasSameStatusInAnySubtask(task)));
 
+  // TODO: Remove
   constructor() {
     effect(() => {
       console.table(this.tasks());
