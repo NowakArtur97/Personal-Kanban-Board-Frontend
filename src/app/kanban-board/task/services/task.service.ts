026bb0bf4a33f22325a53f299944d943d24be24c
arnk <nowart97@gmail.com>
2024-03-30 11:29:34 +0100

    Add more example tasks data

--- src/app/kanban-board/task/services/task.service.ts
+++ src/app/kanban-board/task/services/task.service.ts
@@ -2,15 +2,16 @@ import { Injectable, signal } from '@angular/core';
 import Task from '../models/task.model';
 import TaskPriority from '../models/task-priority.model';
 import { TaskStatus } from '../models/task-status.model';
+import Subtask from '../models/subtask.model';
 
 const EXAMPLE_TASK: Task = {
     id: "3e0fb254-60ad-4614-8972-f783e4f62170",
     userId: "3e0fb254-60ad-4614-8972-f783e4f62170",
-    title: "Setup backend repository",
-    description: "Create backend repository for Personal Kanbard Board application",
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+    description: "Aliquam nunc mi, faucibus et justo et, rhoncus finibus ipsum. Aliquam iaculis tempor felis, sit amet rhoncus magna interdum in. Maecenas finibus ut mauris ornare tristique. Donec urna erat, vulputate a ante nec, fringilla porta diam. Morbi eleifend mi nec tortor vestibulum pretium. Donec imperdiet vestibulum vulputate. Phasellus ante diam, dapibus in venenatis et, cursus eu sapien. Nulla quis tincidunt ante, eget sollicitudin lorem.",
     status: TaskStatus.InProgress,
     priority: TaskPriority.MEDIUM,
-    targetEndDate: "",
+    targetEndDate: "2016-08-16T12:10:56",
     createdOn: "Tuesday, Aug 16, 2016 12:10:56 PM",
     createdBy: "Arthur Smith",
     updatedOn: "Tuesday, Aug 16, 2016 12:10:56 PM",
@@ -18,24 +19,75 @@ const EXAMPLE_TASK: Task = {
     assignedTo: "Andrew Smith",
     subtasks: []
 };
+const EXAMPLE_SUBTASK: Subtask = {
+    id: "3e0fb254-60ad-4614-8972-f783e4f62170",
+    userId: "3e0fb254-60ad-4614-8972-f783e4f62170",
+    taskId: "3e0fb254-60ad-4614-8972-f783e4f62170",
+    title: "Curabitur malesuada scelerisque mi, sed rutrum nisi rhoncus eget.",
+    description: "Donec pulvinar at arcu eget venenatis. Fusce et vestibulum odio. Ut aliquam iaculis pharetra. Integer a ipsum eleifend, sodales ex a, facilisis justo. Aliquam vehicula nec libero et gravida. Vivamus laoreet quam ac elit pulvinar, eu finibus odio mollis. Etiam mollis dolor eget feugiat convallis. Sed ac erat eget libero placerat hendrerit id semper tellus. Integer lobortis in quam id bibendum. Duis nec convallis nisl. Praesent elementum lorem felis, et efficitur diam finibus at. Quisque pulvinar dui lacus, quis rhoncus quam euismod porttitor.",
+    status: TaskStatus.InProgress,
+    priority: TaskPriority.MEDIUM,
+    targetEndDate: "2016-08-16T12:10:56",
+    createdOn: "Tuesday, Aug 16, 2016 13:10:56 PM",
+    createdBy: "Arthur Smith",
+    updatedOn: "Tuesday, Aug 16, 2016 13:10:56 PM",
+    updatedBy: "Andrew Smith",
+    assignedTo: "Andrew Smith",
+};
 const TEMP_TASKS: Task[] = [
     {
         ...EXAMPLE_TASK,
-        status: TaskStatus.ReadyToStart,
+        status: TaskStatus.InProgress,
         priority: TaskPriority.MEDIUM,
-        subtasks: []
+        subtasks: [
+            {
+                ...EXAMPLE_SUBTASK,
+                status: TaskStatus.ReadyToStart,
+                priority: TaskPriority.LOW
+            },
+            {
+                ...EXAMPLE_SUBTASK,
+                status: TaskStatus.InProgress,
+                priority: TaskPriority.MEDIUM
+            },
+            {
+                ...EXAMPLE_SUBTASK,
+                status: TaskStatus.Done,
+                priority: TaskPriority.HIGH
+            },
+        ]
     },
     {
         ...EXAMPLE_TASK,
         status: TaskStatus.ReadyToStart,
         priority: TaskPriority.MEDIUM,
-        subtasks: []
+        subtasks: [{
+            ...EXAMPLE_SUBTASK,
+            status: TaskStatus.ReadyToStart,
+            priority: TaskPriority.MEDIUM
+        },
+        {
+            ...EXAMPLE_SUBTASK,
+            status: TaskStatus.ReadyToStart,
+            priority: TaskPriority.HIGH
+        },]
     },
     {
         ...EXAMPLE_TASK,
         status: TaskStatus.InProgress,
         priority: TaskPriority.MEDIUM,
-        subtasks: []
+        subtasks: [
+            {
+                ...EXAMPLE_SUBTASK,
+                status: TaskStatus.ReadyToStart,
+                priority: TaskPriority.LOW
+            },
+            {
+                ...EXAMPLE_SUBTASK,
+                status: TaskStatus.InProgress,
+                priority: TaskPriority.HIGH
+            },
+        ]
     },
     {
         ...EXAMPLE_TASK,
@@ -53,7 +105,18 @@ const TEMP_TASKS: Task[] = [
         ...EXAMPLE_TASK,
         status: TaskStatus.Done,
         priority: TaskPriority.MEDIUM,
-        subtasks: []
+        subtasks: [
+            {
+                ...EXAMPLE_SUBTASK,
+                status: TaskStatus.Done,
+                priority: TaskPriority.HIGH
+            },
+            {
+                ...EXAMPLE_SUBTASK,
+                status: TaskStatus.Done,
+                priority: TaskPriority.HIGH
+            },
+        ]
     },
 ];
 
