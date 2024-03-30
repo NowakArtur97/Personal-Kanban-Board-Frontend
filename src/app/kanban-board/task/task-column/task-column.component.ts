4be1dca30cb5c70de795178df6cc096fa23934ed
arnk <nowart97@gmail.com>
2024-03-30 16:30:14 +0100

    Change filtering of tasks in task column components

--- src/app/kanban-board/task/task-column/task-column.component.ts
+++ src/app/kanban-board/task/task-column/task-column.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, effect, inject, input } from '@angular/core';
 import { TaskStatus } from '../models/task-status.model';
 import { TaskService } from '../services/task.service';
+import Task from '../models/task.model';
 
 @Component({
   selector: 'app-task-column',
@@ -17,13 +18,20 @@ export class TaskColumnComponent {
 
   private tasks = computed(() =>
     this.taskService.tasks().filter(task =>
-      TaskStatus[task.status] === this.status));
+      this.hasSameTaskStatus(task)
+      || this.hasSameStatusInAnySubtask(task)));
 
   constructor() {
     effect(() => {
       console.table(this.tasks());
     });
   }
+
+  private hasSameStatusInAnySubtask = (task: Task): boolean =>
+    task.subtasks.some(subtask => TaskStatus[subtask.status] === this.status);
+
+  private hasSameTaskStatus = (task: Task): boolean =>
+    TaskStatus[task.status] === this.status;
 
   get status(): string {
     return TaskStatus[this.tasksStatus()!];
