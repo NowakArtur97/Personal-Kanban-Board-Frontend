e8bdd31108c1a92ea5f6e2460a9ab96682289b8a
arnk <nowart97@gmail.com>
2024-03-30 10:59:53 +0100

    Generate columns for all tasks statuses

--- src/app/kanban-board/kanban-board.component.ts
+++ src/app/kanban-board/kanban-board.component.ts
@@ -1,12 +1,16 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ALL_TASKS_STATUSES } from './task/models/task-status';
+import { TaskColumnComponent } from './task/task-column/task-column.component';
 
 @Component({
     selector: 'app-kanban-board',
     standalone: true,
-    imports: [],
+    imports: [NgFor, TaskColumnComponent],
     templateUrl: './kanban-board.component.html',
     styleUrl: './kanban-board.component.css'
 })
 export class KanbanBoardComponent {
 
+    tasksStatuses = ALL_TASKS_STATUSES;
 }
