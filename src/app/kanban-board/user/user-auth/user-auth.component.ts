19b1e7efef7598ce8b8667b12184f8288fb84818
arnk <nowart97@gmail.com>
2024-05-02 11:58:11 +0200

    Manage change action buttons hover styles based on current action

--- src/app/kanban-board/user/user-auth/user-auth.component.ts
+++ src/app/kanban-board/user/user-auth/user-auth.component.ts
@@ -15,4 +15,18 @@ export class UserAuthComponent {
   changeAction(): void {
     this.isInLoginView = !this.isInLoginView;
   }
+
+  setupHover(hoveredElement: HTMLButtonElement, inactiveElement: HTMLButtonElement): void {
+    if ((hoveredElement.textContent?.includes("Sign in") && !this.isInLoginView) || (hoveredElement.textContent?.includes("Sign up") && this.isInLoginView)) {
+      hoveredElement.classList.add("user_auth__action_button--active");
+      inactiveElement.classList.remove("user_auth__action_button--active");
+    }
+  }
+
+  setupUnhover(unhoveredElement: HTMLButtonElement, inactiveElement: HTMLButtonElement): void {
+    if ((unhoveredElement.textContent?.includes("Sign in") && !this.isInLoginView) || (unhoveredElement.textContent?.includes("Sign up") && this.isInLoginView)) {
+      unhoveredElement.classList.remove("user_auth__action_button--active");
+      inactiveElement.classList.add("user_auth__action_button--active");
+    }
+  }
 }
