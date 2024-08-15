a40ca2098b51894239ee3ed8a60601b982fd9fa2
arnk <nowart97@gmail.com>
2024-08-15 17:02:27 +0200

    Create form group for task form

--- src/app/kanban-board/task/task-form/task-form.component.ts
+++ src/app/kanban-board/task/task-form/task-form.component.ts
@@ -1,12 +1,29 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [],
+  imports: [NgClass, ReactiveFormsModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
 export class TaskFormComponent {
 
+  taskForm = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(100),
+    ]),
+    description: new FormControl('', [
+      Validators.maxLength(1000),
+    ]),
+    status: new FormControl('In progress'),
+    priority: new FormControl('Low'),
+    targetEndDate: new FormControl(new Date().toISOString().substring(0, 10)),
+    assignedTo: new FormControl('')
+  }
+  );
 }
