9c9c88bc3335771522b53c40381aafaac0cae261
NowakArtur97 <nowart97@gmail.com>
2025-07-17 15:53:22 +0200

    Rename folder containing task related resolvers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
 import { UserAuthComponent } from './kanban-board/user/user-auth/user-auth.component';
-import { tasksResolver } from './kanban-board/task/guards/tasks.resolver';
+import { tasksResolver } from './kanban-board/task/resolvers/tasks.resolver';
 import { isAuthenticated } from './kanban-board/user/guards/is-authenticated.guard';
 
 const title = 'Personal Kanban Board';
