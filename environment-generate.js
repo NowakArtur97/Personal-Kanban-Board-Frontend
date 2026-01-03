6f575042723e41cce88706b73c893ca62de5ed8c
NowakArtur97 <nowart97@gmail.com>
2026-01-03 12:04:21 +0100

    Make sure that environment variables have correct protocols

--- environment-generate.js
+++ environment-generate.js
@@ -1,9 +1,24 @@
+function ensureProtocol(url, protocol) {
+  if (!url) return "";
+  if (
+    url.startsWith("http://") ||
+    url.startsWith("https://") ||
+    url.startsWith("ws://") ||
+    url.startsWith("wss://")
+  ) {
+    return url;
+  }
+  return `${protocol}://${url}`;
+}
+
 function generateEnvironmentContent() {
+  const backendURL = ensureProtocol(process.env.BACKEND_URL, "http");
+  const wsURL = ensureProtocol(process.env.WS_URL, "ws");
   return `export const environment = {
     production: ${process.env.IS_PRODUCTION || false},
     environment: "${process.env.ENVIRONMENT || "local"}",
-    backendURL: "${process.env.BACKEND_URL || "http://localhost:8080"}",
-    wsURL: "${process.env.WS_URL || "ws://localhost:8080/graphql"}"
+    backendURL: "${backendURL || "http://localhost:8080"}",
+    wsURL: "${wsURL || "ws://localhost:8080/graphql"}"
   };`;
 }
 
@@ -11,6 +26,7 @@ function generateEnvironmentContent() {
   const fs = require("fs");
   const fileName = "environment.ts";
   const content = generateEnvironmentContent();
+  console.log(`env : ${content}`);
   process.chdir("src/environments");
   fs.writeFile(fileName, content, (err) => {
     err ? console.log(err) : console.log("env is generated");
