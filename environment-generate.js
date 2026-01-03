9e341629f6be0e364314d3ffb2e0f238195b8c3c
NowakArtur97 <nowart97@gmail.com>
2026-01-03 11:34:56 +0100

    Fix issue with generating environment

--- environment-generate.js
+++ environment-generate.js
@@ -2,7 +2,7 @@ function generateEnvironmentContent() {
   return `export const environment = {
     production: ${process.env.IS_PRODUCTION || false},
     environment: "${process.env.ENVIRONMENT || "local"}",
-    backendURL: "${process.env.BACKEND_URL || "http://localhost:8080"}"
+    backendURL: "${process.env.BACKEND_URL || "http://localhost:8080"}",
     wsURL: "${process.env.WS_URL || "ws://localhost:8080/graphql"}"
   };`;
 }
